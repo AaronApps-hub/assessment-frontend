d95efc45766edbc45b38f88a9eb71f644d55a360
AaronApps-hub <cassmiro2019@gmail.com>
2026-03-05 05:00:01 +0200

    Create assessmentService.js

---
+++ assessmentService.js
@@ -0,0 +1,16 @@
+const KEY = "assessments";
+
+export const getAssessments = () => {
+  return JSON.parse(localStorage.getItem(KEY)) || [];
+};
+
+export const saveAssessment = (assessment) => {
+  const data = getAssessments();
+  data.push(assessment);
+  localStorage.setItem(KEY, JSON.stringify(data));
+};
+
+export const deleteAssessment = (id) => {
+  const data = getAssessments().filter(a => a.id !== id);
+  localStorage.setItem(KEY, JSON.stringify(data));
+};
